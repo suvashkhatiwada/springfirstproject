4da7b8403c1059223bf12334544b1a2e50e287af
Suvash Khatiwada <suvashkhatiwada@gmail.com>
2019-04-17 20:40:11 -0400

    Second Commit

---
+++ src/main/java/com/springfirstproject/maven/SpringFirstProject/App.java
@@ -0,0 +1,13 @@
+package com.springfirstproject.maven.SpringFirstProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/springfirstproject/maven/SpringFirstProject/Customers.java
@@ -0,0 +1,41 @@
+package com.springfirstproject.maven.SpringFirstProject;
+
+public class Customers {
+	private int id;
+	private String fullName;
+	private String email;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	private String address;
+}
