e99f5bb8a1d08f0489cf944071a3a3ce4c840422
Suvash Khatiwada <suvashkhatiwada@gmail.com>
2019-04-18 21:02:06 -0400

    some changes

--- src/main/java/com/springfirstproject/maven/SpringFirstProject/SpringMainApp.java
+++ src/main/java/com/springfirstproject/maven/SpringFirstProject/SpringMainApp.java
@@ -1,20 +1,19 @@
 package com.springfirstproject.maven.SpringFirstProject;
 
-
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringMainApp {
 
 	public static void main(String[] args) {
-		
+
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("application-context.xml");
 		Customers customer = (Customers) ctx.getBean("cust");
-		
-Address address = (Address) ctx.getBean("address");
-		
+
+		Address address = (Address) ctx.getBean("address");
+
 		System.out.println(address.toString());
-		
+
 		System.out.println(customer.toString());
 	}
 
